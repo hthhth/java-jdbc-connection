03479e8726a843eb78997b1ee48a7f787f5564ef
hthhth <54969380+hthhth@users.noreply.github.com>
2021-12-19 01:00:39 +0700

    PreparedStatement

--- databaseTesting/jdbcTest/SQLServerJTDSTest.java
+++ databaseTesting/jdbcTest/SQLServerJTDSTest.java
@@ -3,49 +3,32 @@ package jdbcTest;
 
 import jdbcConnection.SQLServerJTDSConnection;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class SQLServerJTDSTest {
     public static void main(String[] args) throws SQLException {
         Connection conn = SQLServerJTDSConnection.getSQLServerConnection();
         System.out.println("Opened connection: " + conn);
 
-        Statement statement = conn.createStatement();
+        String sqlParam = "SELECT [EMP_ID], [FIRST_NAME], [TITLE], [DEPT_ID] from [automationfc].[dbo].[EMPLOYEE] where [TITLE] like ? and [DEPT_ID] = ?";
 
-        String insertValue = "insert into [dbo].[BRANCH] ([ADDRESS], [CITY], [NAME], [STATE], [ZIP_CODE]) values ('100 Winter Road', 'NY', 'Main', 'MS', '08719')";
-        int rowCount = statement.executeUpdate(insertValue);
-        System.out.println("Row count affected " + rowCount);
+        PreparedStatement preparedStatement = conn.prepareStatement(sqlParam);
+        preparedStatement.setString(1, "%ent");
+        preparedStatement.setInt(2,3);
 
-        String sql = "select * from [automationfc].[dbo].[branch]";
-        ResultSet resultSet = statement.executeQuery(sql);
+        ResultSet resultSet = preparedStatement.executeQuery();
 
-//        while (resultSet.next()) {
-//            int empID = resultSet.getInt(1);
-//            String empFirstName = resultSet.getString("first_name");
-//            String empLastName = resultSet.getString("last_name");
-//            int deptID = resultSet.getInt(4);
-//
-//            System.out.println("-----------------------------");
-//            System.out.println("Employee ID: " + empID);
-//            System.out.println("Employee first name: " + empFirstName);
-//            System.out.println("Employee last name: " + empLastName);
-//            System.out.println("Dept ID: " + deptID);
-//
-//        }
         while (resultSet.next()) {
-            int branchID = resultSet.getInt(1);
-            String branchAddress = resultSet.getString("ADDRESS");
-            String branchCity = resultSet.getString("CITY");
-            String branchName = resultSet.getString("NAME");
+            int empID = resultSet.getInt(1);
+            String empFirstName = resultSet.getString("first_name");
+            String empTitle = resultSet.getString("TITLE");
+            int deptID = resultSet.getInt(4);
 
             System.out.println("-----------------------------");
-            System.out.println("Branch ID: " + branchID);
-            System.out.println("Branch address: " + branchAddress);
-            System.out.println("Branch city: " + branchCity);
-            System.out.println("Branch name: " + branchName);
+            System.out.println("Employee ID: " + empID);
+            System.out.println("Employee first name: " + empFirstName);
+            System.out.println("Employee title: " + empTitle);
+            System.out.println("Dept ID: " + deptID);
 
         }
         conn.close();
