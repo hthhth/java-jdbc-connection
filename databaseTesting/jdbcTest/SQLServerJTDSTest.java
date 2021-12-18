788b3f73a2fb44e03ba01a6b1453da9991394bb1
hthhth <54969380+hthhth@users.noreply.github.com>
2021-12-19 00:32:34 +0700

    Insert value into table

--- databaseTesting/jdbcTest/SQLServerJTDSTest.java
+++ databaseTesting/jdbcTest/SQLServerJTDSTest.java
@@ -14,20 +14,38 @@ public class SQLServerJTDSTest {
         System.out.println("Opened connection: " + conn);
 
         Statement statement = conn.createStatement();
-        String sql = "select emp_id, first_name, last_name, dept_id from [automationfc].[dbo].[employee]";
+
+        String insertValue = "insert into [dbo].[BRANCH] ([ADDRESS], [CITY], [NAME], [STATE], [ZIP_CODE]) values ('100 Winter Road', 'NY', 'Main', 'MS', '08719')";
+        int rowCount = statement.executeUpdate(insertValue);
+        System.out.println("Row count affected " + rowCount);
+
+        String sql = "select * from [automationfc].[dbo].[branch]";
         ResultSet resultSet = statement.executeQuery(sql);
 
+//        while (resultSet.next()) {
+//            int empID = resultSet.getInt(1);
+//            String empFirstName = resultSet.getString("first_name");
+//            String empLastName = resultSet.getString("last_name");
+//            int deptID = resultSet.getInt(4);
+//
+//            System.out.println("-----------------------------");
+//            System.out.println("Employee ID: " + empID);
+//            System.out.println("Employee first name: " + empFirstName);
+//            System.out.println("Employee last name: " + empLastName);
+//            System.out.println("Dept ID: " + deptID);
+//
+//        }
         while (resultSet.next()) {
-            int empID = resultSet.getInt(1);
-            String empFirstName = resultSet.getString("first_name");
-            String empLastName = resultSet.getString("last_name");
-            int deptID = resultSet.getInt(4);
+            int branchID = resultSet.getInt(1);
+            String branchAddress = resultSet.getString("ADDRESS");
+            String branchCity = resultSet.getString("CITY");
+            String branchName = resultSet.getString("NAME");
 
             System.out.println("-----------------------------");
-            System.out.println("Employee ID: " + empID);
-            System.out.println("Employee first name: " + empFirstName);
-            System.out.println("Employee last name: " + empLastName);
-            System.out.println("Dept ID: " + deptID);
+            System.out.println("Branch ID: " + branchID);
+            System.out.println("Branch address: " + branchAddress);
+            System.out.println("Branch city: " + branchCity);
+            System.out.println("Branch name: " + branchName);
 
         }
         conn.close();
